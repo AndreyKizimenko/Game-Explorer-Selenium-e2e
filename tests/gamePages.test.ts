93b24644a9339ed932bca2ce5aa3a5fb7a2e8a98
AndreyKizimenko <andreykizimenko@gmail.com>
2024-04-10 22:24:11 -0500

    updated a few test case titles and added additional comments in the gamePage checks for easier maintenance and readability

--- tests/gamePages.test.ts
+++ tests/gamePages.test.ts
@@ -7,10 +7,11 @@ import initialChromeStartup from "../hooks/testStartup";
 
 describe("Individual game pages", async () => {
   let driver: WebDriver;
+  // number of games to check on each test case
   const gamesToCheck = 5;
 
   beforeEach(async () => {
-    driver = await initialChromeStartup()
+    driver = await initialChromeStartup();
   });
   afterEach(async () => {
     await driver.sleep(1000);
@@ -21,11 +22,14 @@ describe("Individual game pages", async () => {
     let gameGrid = await getGameCards(driver);
 
     for (let i = 0; i < gamesToCheck; i++) {
+      // selecting a game in the grid, saving its href and navigating to its page
       const href = await gameGrid[i].getAttribute("href");
       await gameGrid[i].click();
       await driver.wait(until.elementLocated(By.css("h2.css-1dklj6k"))).getText();
+      // asserting that the url matches the card that was selected
       const currentURL = await driver.getCurrentUrl();
       currentURL.should.be.equal(href);
+      // navigating back to the home page
       const logo = await driver.findElement(By.css("nav.css-134xcib a"));
       await logo.click();
       gameGrid = await driver.wait(until.elementsLocated(By.css(".css-1hmna4a a")), 20000);
@@ -58,12 +62,14 @@ describe("Individual game pages", async () => {
         .wait(until.elementLocated(By.css(".css-1v8my8o p")))
         .getText();
 
+      // locating and expanding the text description
       await driver.findElement(By.css(".css-1v8my8o button")).click();
       const expandedDescription = await driver
         .wait(until.elementLocated(By.css(".css-1v8my8o p")))
         .getText();
       expandedDescription.length.should.be.greaterThan(gameDescription.length);
 
+      // collapsing the text and comparing again
       await driver.findElement(By.css(".css-1v8my8o button")).click();
       const collapsedDescription = await driver
         .wait(until.elementLocated(By.css(".css-1v8my8o p")))
@@ -82,12 +88,12 @@ describe("Individual game pages", async () => {
     for (let i = 0; i < gamesToCheck; i++) {
       await gameGrid[i].click();
       const dataList = await driver.wait(until.elementsLocated(By.css("dl > div:nth-child(1) p")));
-
+      // itterating through a list of platforms
       let platforms: string[] = [];
       for (let plat of dataList) {
         platforms.push(await plat.getText());
       }
-
+      // confirming that the displayed platforms match supported platforms
       PLATFORMS.should.include.members(platforms);
 
       const logo = await driver.findElement(By.css("nav.css-134xcib a"));
@@ -102,12 +108,12 @@ describe("Individual game pages", async () => {
     for (let i = 0; i < gamesToCheck; i++) {
       await gameGrid[i].click();
       const dataList = await driver.wait(until.elementsLocated(By.css("dl > div:nth-child(3) p")));
-
+      // itterating through a list of genres
       let genres: string[] = [];
       for (let genre of dataList) {
         genres.push(await genre.getText());
       }
-
+      // confirming that the displayed genres match supported genres
       GENRES.should.include.members(genres);
 
       const logo = await driver.findElement(By.css("nav.css-134xcib a"));
@@ -121,6 +127,7 @@ describe("Individual game pages", async () => {
 
     for (let i = 0; i < gamesToCheck; i++) {
       await gameGrid[i].click();
+      // asserting that the score displayed is between 0 and 101
       const metaScore = await driver
         .wait(until.elementLocated(By.css("dl > div:nth-child(2) span")))
         .getText();
@@ -143,8 +150,8 @@ describe("Individual game pages", async () => {
       for (let publisher of dataList) {
         publishers.push(await publisher.getText());
       }
-
-      publishers.should.not.be.empty
+      // asserting that the publishers section is populated correctly
+      publishers.should.not.be.empty;
 
       const logo = await driver.findElement(By.css("nav.css-134xcib a"));
       await logo.click();
